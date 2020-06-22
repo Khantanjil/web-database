387a6ccd79103ddddd79d7957dfc2585075ee930
Khantanjil <tanjilkhan.sh@gmail.com>
2020-06-22 14:29:24 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from send_email import send_email
+import email
+
+app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/height_collector'
+db = SQLAlchemy(app)
+class Data(db.Model):
+    __tablename__ = "data"
+    id=db.Column(db.Integer, primary_key=True)
+    email_ = db.Column(db.String(120), unique=True)
+    height_ = db.Column(db.Integer)
+
+    def __init__(self, email_, height_):
+        self.email_ = email_
+        self.height_ = height_
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/success", methods=['POST'])
+def success():
+    if request.method == 'POST':
+        email = request.form["email_name"]
+        print(email)
+        height = request.form["height_name"]
+        print(height)
+        send_email(email, height)
+        if db.session.query(Data).filter(Data.email_ == email).count() == 0:
+            data = Data(email,height)
+            db.session.add(data)
+            db.session.commit()
+            return render_template("success.html")
+    return render_template("index.html", text="Seems like we've got something from that email address.")
+
+if __name__ == "__main__":
+    app.debug=True
+    app.run()
